64beb9e9a67860420ba7cf750ce53c0ac6e0453a
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2021-03-12 12:16:40 +0530

    Basic config of testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+import EnzymeAdapater from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+    adapter:new EnzymeAdapater(),
+    disableLifecycleMethods:true
+})
