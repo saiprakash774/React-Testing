45579e78dd9f931ad040bf421164665aca4f2c2c
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2021-03-18 00:05:23 +0530

    simulat click event on button component

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -16,22 +16,30 @@ describe(' SharedButton Component',()=>{
             expect(propsError).toBeUndefined();
         });
 
+        // ||> checking for the correct property types
     });
          describe('Renders',()=>{
         let wrapper;
+        let mockFunc;
         beforeEach(()=>{
+            mockFunc=jest.fn();
             const props={
                 buttonText:'Example Button Text',
-                emitEvent:()=>{
-
-                }
+                emitEvent: mockFunc
             };
             wrapper=shallow(<SharedButton {...props}/>);
         });
-
+//component renders without error
         it('Should Render a button',()=>{
             const button=findByTestAttr(wrapper,'buttonComponent');
             expect(button.length).toBe(1);
         });
+
+        it('Should emit callback on click event',()=>{
+            const button=findByTestAttr(wrapper,'buttonComponent');
+            button.simulate('click');
+           const callback=mockFunc.mock.calls.length;
+           expect(callback).toBe(1); 
+        })
     });
 });
